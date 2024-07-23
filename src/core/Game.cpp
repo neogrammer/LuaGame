7e533dee8208dbaf0dae6dc0e425f03288804d76
Justin Hardee <JHARDEE@augusta.edu>
2024-07-23 14:32:57 -0400

    Level creation  system handled in lua scripts

--- src/core/Game.cpp
+++ src/core/Game.cpp
@@ -5,6 +5,104 @@ void Game::update(float dt)
 }
 void Game::render()
 {
+	sf::Vector2i tile = { 0, 0 };
+	for (tile.y = 0; tile.y < mLevelSize.y; tile.y++)
+	{
+		for (tile.x = 0; tile.x < mLevelSize.x; tile.x++)
+		{
+			TileType b = mLevelVec[(uint8_t)tile.y * mLevelSize.x + tile.x];
+
+
+			switch (b)
+			{
+			case TileType::Empty:
+				break;
+			case TileType::Solid:
+			{
+				sf::Sprite spr{};
+				spr.setTexture(tex);
+				spr.setTextureRect({ {7 * 32, 0}, {32, 32} });
+				spr.setPosition({ (float)tile.x * 32.f, (float)tile.y * 32.f });
+				mWnd.draw(spr);
+			}
+			break;
+			case TileType::TL:
+			{
+				sf::Sprite spr{};
+				spr.setTexture(tex);
+				spr.setTextureRect({ {0 * 32, 0}, {32, 32} });
+				spr.setPosition({ (float)tile.x * 32.f, (float)tile.y * 32.f });
+				mWnd.draw(spr);
+			}
+			break;
+			case TileType::TOP:
+			{
+				sf::Sprite spr{};
+				spr.setTexture(tex);
+				spr.setTextureRect({ {1 * 32, 0}, {32, 32} });
+				spr.setPosition({ (float)tile.x * 32.f, (float)tile.y * 32.f });
+				mWnd.draw(spr);
+			}
+			break;
+			case TileType::TR:
+			{
+				sf::Sprite spr{};
+				spr.setTexture(tex);
+				spr.setTextureRect({ {2 * 32, 0}, {32, 32} });
+				spr.setPosition({ (float)tile.x * 32.f, (float)tile.y * 32.f });
+				mWnd.draw(spr);
+			}
+			break;
+			case TileType::L:
+			{
+				sf::Sprite spr{};
+				spr.setTexture(tex);
+				spr.setTextureRect({ {6 * 32, 0}, {32, 32} });
+				spr.setPosition({ (float)tile.x * 32.f, (float)tile.y * 32.f });
+				mWnd.draw(spr);
+			}
+			break;
+			case TileType::R:
+			{
+				sf::Sprite spr{};
+				spr.setTexture(tex);
+				spr.setTextureRect({ {0 * 32, 32}, {32, 32} });
+				spr.setPosition({ (float)tile.x * 32.f, (float)tile.y * 32.f });
+				mWnd.draw(spr);
+			}
+			break;
+			case TileType::BL:
+			{
+				sf::Sprite spr{};
+				spr.setTexture(tex);
+				spr.setTextureRect({ {96, 64}, {32, 32} });
+				spr.setPosition({ (float)tile.x * 32.f, (float)tile.y * 32.f });
+				mWnd.draw(spr);
+			}
+			break;
+			case TileType::B:
+			{
+				sf::Sprite spr{};
+				spr.setTexture(tex);
+				spr.setTextureRect({ {32, 64}, {32, 32} });
+				spr.setPosition({ (float)tile.x * 32.f, (float)tile.y * 32.f });
+				mWnd.draw(spr);
+			}
+			break;
+			case TileType::BR:
+			{
+				sf::Sprite spr{};
+				spr.setTexture(tex);
+				spr.setTextureRect({ {64, 64}, {32, 32} });
+				spr.setPosition({ (float)tile.x * 32.f, (float)tile.y * 32.f });
+				mWnd.draw(spr);
+			}
+			break;
+			default:
+				break;
+			}
+		}
+	}
 	sf::Text player1Text;
 	player1Text.setFont(Cfg::fonts.get((int)Cfg::Fonts::FriskyPuppy));
 	player1Text.setCharacterSize(44U);
@@ -24,23 +122,27 @@ void Game::render()
 }
 
 Game::Game()
-	: mWnd{ sf::VideoMode{1600U,900U,32U},"LuaGame",sf::Style::Close }
+	: mWnd{ sf::VideoMode{1280U,768U,32U},"LuaGame",sf::Style::Close }
 	, player{}
 	, player2{}
+	, tex{}
 {
 	mWnd.setPosition({ 100,55 });
 	mGameRunning = true;
+	tex.loadFromFile("assets/textures/tilesets/map1/platformer1.png");
 }
 
 Game::Game(sf::ContextSettings& settings)
-	: mWnd{ sf::VideoMode{1600U,900U,32U},"LuaGame",sf::Style::Close, settings}
+	: mWnd{ sf::VideoMode{1280U,768U,32U},"LuaGame",sf::Style::Close, settings }
 	, player{}
 	, player2{}
+	, tex{}
 {
 	mWnd.setPosition({ 100,55 });
 	mGameRunning = true;
+	tex.loadFromFile("assets/textures/tilesets/map1/platformer1.png");
 
-	
+
 }
 
 void Game::run()
@@ -149,17 +251,20 @@ void Game::run()
 
 	lua_State* Lvl = luaL_newstate();
 	luaL_openlibs(Lvl);
-	lua_register(L, "cpp_loadLevel", lua_loadLevel);
+	lua_register(Lvl, "cpp_loadLevel", lua_loadLevel);
+	lua_register(Lvl, "cpp_setTile", lua_setTile);
+
 	if (mylua::CheckLua(Lvl, luaL_dofile(Lvl, "assets/lua_scripts/level.lua")))
 	{
 		lua_getglobal(Lvl, "LoadLevel");
 		if (lua_isfunction(Lvl, -1))
 		{
-			lua_pushlightuserdata(Lvl,this);
+			lua_pushlightuserdata(Lvl, this);
 			lua_pushnumber(Lvl, 1);
 			if (mylua::CheckLua(Lvl, lua_pcall(Lvl, 2, 1, 0)))
 			{
 				std::cout << "Success" << std::endl;
+
 			}
 			lua_pop(Lvl, 1);
 		}
@@ -213,21 +318,59 @@ void Game::run()
 	}
 }
 
-int Game::lua_loadLevel(lua_State* L)
-{
-	if (lua_gettop(L) != 3) return -1;
-	Game* obj = static_cast<Game*>(lua_touserdata(L, 1));
-	int w = lua_tointeger(L, 2);
-	int h = lua_tointeger(L, 3);
-	obj->loadLevel(w, h);
-	return 0;
-}
 
 void Game::loadLevel(int w, int h)
 {
 	mLevelSize = { w, h };
 	mLevelVec.clear();
-	mLevelVec.resize(w*h);
+	mLevelVec.resize((uint8_t)w * h);
+	for (int y = 0; y < h; y++)
+	{
+		for (int x = 0; x < w; x++)
+		{
+			mLevelVec.push_back(TileType::Empty);
+		}
+	}
 
 	std::cout << w << " " << h << std::endl;
+}
+
+void Game::setTile(int x, int y, int str)
+{
+
+	switch (str)
+	{
+	case 0:
+		mLevelVec[(uint8_t)y * mLevelSize.x + x] = TileType::Empty;
+		break;
+	case 1:
+		mLevelVec[(uint8_t)y * mLevelSize.x + x] = TileType::Solid;
+		break;
+	case 2:
+		mLevelVec[(uint8_t)y * mLevelSize.x + x] = TileType::TL;
+		break;
+	case 3:
+		mLevelVec[(uint8_t)y * mLevelSize.x + x] = TileType::TOP;
+		break;
+	case 4:
+		mLevelVec[(uint8_t)y * mLevelSize.x + x] = TileType::TR;
+		break;
+	case 5:
+		mLevelVec[(uint8_t)y * mLevelSize.x + x] = TileType::L;
+		break;
+	case 6:
+		mLevelVec[(uint8_t)y * mLevelSize.x + x] = TileType::R;
+		break;
+	case 7:
+		mLevelVec[(uint8_t)y * mLevelSize.x + x] = TileType::BL;
+		break;
+	case 8:
+		mLevelVec[(uint8_t)y * mLevelSize.x + x] = TileType::B;
+		break;
+	case 9:
+		mLevelVec[(uint8_t)y * mLevelSize.x + x] = TileType::BR;
+		break;
+	default:
+		break;
+	}
 }
--- src/core/Game.hpp
+++ src/core/Game.hpp
@@ -6,9 +6,13 @@
 #include <SFML/Window/ContextSettings.hpp>
 #include <SFML/System/Vector2.hpp>
 
+
 extern "C"
 {
-	struct lua_State;
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+#include <string>
 };
 
 class Game
@@ -24,10 +28,20 @@ class Game
 	enum class TileType : int
 	{
 		Empty = 0,
-		Solid
+		Solid = 1,
+		TL = 2,
+		TOP = 3,
+		TR = 4,
+		L = 5,
+		R = 6,
+		BL = 7,
+		B = 8,
+		BR = 9
 	};
 	sf::Vector2i mLevelSize{};
 	std::vector<TileType> mLevelVec{};
+
+	sf::Texture tex{};
 
 
 	sf::RenderWindow mWnd;
@@ -47,8 +61,33 @@ public:
 	void run();
 
 	void loadLevel(int w, int h);
-	static int lua_loadLevel(lua_State* L);
-	
+	static int lua_loadLevel(lua_State* L)
+	{
+		if (lua_gettop(L) != 3) return -1;
+		Game* obj = static_cast<Game*>(lua_touserdata(L, 1));
+		int w = (int)lua_tointeger(L, 2);
+		int h = (int)lua_tointeger(L, 3);
+		obj->loadLevel(w, h);
+		return 0;
+	};
+
+	void setTile(int x, int y, int str);
+
+	static int lua_setTile(lua_State* L)
+	{
+		if (lua_gettop(L) != 4) return -1;
+		Game* obj = static_cast<Game*>(lua_touserdata(L, 1));
+		int x = (int)lua_tointeger(L, 2);
+		int y = (int)lua_tointeger(L, 3);
+		int type = (int)lua_tonumber(L, 4);
+		obj->setTile(x, y, type);
+
+
+		return 0;
+	};
+
+
+
 };
 
 #endif
